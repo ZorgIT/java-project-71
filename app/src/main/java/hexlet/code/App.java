7ae8154c0bfdcc0e53499e0e75180fedb610242d
Zorgit <piligrimexe@gmail.com>
2023-12-13 23:45:45 +0300

    Add picocli base info for gendiff app with dependencies

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    boolean helpRequested;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    boolean versionRequested;
+
+    @Override
+    public void run() {
+        // Your code to compare and show the difference of two configuration files goes here...
+        Differ.generate();
+        System.out.println("Comparing two configuration files...");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CommandLine commandLine = new CommandLine(new App());
+        commandLine.addSubcommand("help", new CommandLine.HelpCommand()); // Add a subcommand for displaying help
+        int exitCode = commandLine.execute(args);
+        System.exit(exitCode);
     }
 }
