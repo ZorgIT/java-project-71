ad182a85e30714f517e27b12f9a82d46fa2bf17f
Zorgit <piligrimexe@gmail.com>
2023-12-15 23:24:31 +0300

    CheckStyle update

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,23 +9,28 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 
-@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
-        description = "Compares two configuration files and shows a " +
-                "difference.")
-public class App implements Runnable {
-    @CommandLine.Parameters(index ="0", description = "Path to the first file")
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a "
+                + "difference.")
+public final class App implements Runnable {
+    @CommandLine.Parameters(index = "0",
+            description = "Path to the first file")
     private String filePath1;
-    @CommandLine.Parameters(index ="1", description = "Path to the second file")
+    @CommandLine.Parameters(index = "1",
+            description = "Path to the second file")
     private String filePath2;
 
-    @Option(names = {"-f", "--format"}, description = "Output format " +
-            "[default: stylish]")
-    String format = "stylish";
-    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
-    boolean helpRequested;
+    @Option(names = {"-f", "--format"}, description = "Output format "
+            + "[default: stylish]")
+    private String format = "stylish";
+    @Option(names = {"-h", "--help"}, usageHelp = true,
+            description = "Show this help message and exit.")
+    private boolean helpRequested;
 
-    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
-    boolean versionRequested;
+    @Option(names = {"-V", "--version"}, versionHelp = true,
+            description = "Print version information and exit.")
+    private boolean versionRequested;
 
     @Override
     public void run() {
@@ -33,17 +38,19 @@ public class App implements Runnable {
             String file1 = readFile(filePath1);
             String file2 = readFile(filePath2);
             Differ.generate(file1, file2);
-        } catch (Exception e ){
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    private String readFile(String filePath) throws IOException {
+    private String readFile(final String filePath) throws IOException {
         return Files.readString(Paths.get(filePath));
     }
-    public static void main(String[] args) {
+
+    public static void main(final String[] args) {
         CommandLine commandLine = new CommandLine(new App());
-        commandLine.addSubcommand("help", new CommandLine.HelpCommand()); // Add a subcommand for displaying help
+        // Add a subcommand for displaying help
+        commandLine.addSubcommand("help", new CommandLine.HelpCommand());
         int exitCode = commandLine.execute(args);
         System.exit(exitCode);
     }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,12 +1,19 @@
 package hexlet.code;
 
-import java.util.*;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class Differ {
-    public static void generate(String json1, String json2) throws Exception {
+final class Differ {
+    private Differ() {
+    }
+
+    public static void generate(final String json1,
+                                final String json2) throws Exception {
         Map<String, Object> map1 = getData(json1);
         Map<String, Object> map2 = getData(json2);
         List<List<String>> result = checkData(map1, map2);
@@ -19,16 +26,17 @@ public class Differ {
         System.out.println("}");
     }
 
-    public static Map getData(String content) throws Exception {
+    public static Map getData(final String content) throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         Map<String, Object> map
-                = objectMapper.readValue(content, new TypeReference<Map<String, Object>>() {
+                = objectMapper.readValue(content,
+                new TypeReference<Map<String, Object>>() {
         });
         return map;
     }
 
-    public static List lineAdd(String val1, String val2,
-                               Object val3) {
+    public static List lineAdd(final String val1, final String val2,
+                               final Object val3) {
         List<Object> line = new ArrayList<>();
         line.add(val1);
         line.add(val2);
@@ -36,8 +44,8 @@ public class Differ {
         return line;
     }
 
-    public static List<List<String>> checkData(Map<String, Object> oldMap,
-                                               Map<String,
+    public static List<List<String>> checkData(final Map<String, Object> oldMap,
+                                               final Map<String,
                                                        Object> newMap) {
         List<List<String>> lineStatus = new ArrayList<>();
         // 1. идем по старому файлу через ентрисет, задаем переменные
@@ -47,11 +55,13 @@ public class Differ {
             String curKey = entryOld.getKey();
             //2. проверяем, есть ли такой ключ в файле 2
             if (newValue != null) {
-                //в2 Такой ключ есть, доп проверка - ключ имеет одинаковые значения
+                //в2 Такой ключ есть, доп проверка
+                // - ключ имеет одинаковые значения
                 if (newValue.equals(oldValue)) {
                     lineStatus.add(lineAdd(" ", curKey, oldValue));
                 } else {
-                    //  в3 Такой ключ есть, доп проверка - ключи имеют разные значения = установить
+                    //  в3 Такой ключ есть, доп проверка
+                    //  - ключи имеют разные значения = установить
                     lineStatus.add(lineAdd("-", curKey, oldValue));
                     lineStatus.add(lineAdd("+", curKey, newValue));
                 }
