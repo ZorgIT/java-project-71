23bee92aeff930ce2864a00ddbe747b458bcf4fe
Zorgit <piligrimexe@Gmail.com>
2023-12-29 23:11:33 +0300

    Improve generate method (genereate return List result). checkstyle improve

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -8,26 +8,22 @@ import java.util.Comparator;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 final class Differ {
     private Differ() {
     }
 
-    public static void generate(final String json1,
-                                final String json2) throws Exception {
-        Map<String, Object> map1 = getData(json1);
-        Map<String, Object> map2 = getData(json2);
+    public static List<List<String>> generate(final String json1,
+                                              final String json2)
+            throws Exception {
+        Map<String, Object> map1 = getJsonData(json1);
+        Map<String, Object> map2 = getJsonData(json2);
         List<List<String>> result = checkData(map1, map2);
-
-        System.out.println("\n{");
-        for (List<String> line : result) {
-            System.out.printf("  %s %s: %s \n", line.get(0), line.get(1),
-                    line.get(2));
-        }
-        System.out.println("}");
+        return result;
     }
 
-    public static Map<String, Object> getData(final String content) {
+    public static Map<String, Object> getJsonData(final String content) {
         System.out.println("Received JSON content: " + content);
 
         try {
@@ -44,9 +40,26 @@ final class Differ {
         }
     }
 
+    public static Map<String, Object> getYmlData(final String content) {
+        System.out.println("Received Yml content: " + content);
+
+        try {
+            ObjectMapper objectMapper = new YAMLMapper();
+            Map<String, Object> map = objectMapper.readValue(content,
+                    new TypeReference<Map<String, Object>>() {
+                    });
+            System.out.println("YAML parsed successfully.");
+            return map;
+        } catch (IOException e) {
+            System.out.println("Error parsing YAML: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 
     public static List<String> lineAdd(final String val1, final String val2,
-                               final Object val3) {
+                                       final Object val3) {
         List<String> line = new ArrayList<>();
         line.add(val1);
         line.add(val2);
@@ -58,35 +71,24 @@ final class Differ {
                                                final Map<String,
                                                        Object> newMap) {
         List<List<String>> lineStatus = new ArrayList<>();
-        // 1. идем по старому файлу через ентрисет, задаем переменные
+
         for (Map.Entry<String, Object> entryOld : oldMap.entrySet()) {
             Object newValue = newMap.get(entryOld.getKey());
             Object oldValue = entryOld.getValue();
             String curKey = entryOld.getKey();
-            //2. проверяем, есть ли такой ключ в файле 2
-            if (newValue != null) {
-                //в2 Такой ключ есть, доп проверка
-                // - ключ имеет одинаковые значения
-                if (newValue.equals(oldValue)) {
-                    lineStatus.add(lineAdd(" ", curKey, oldValue));
-                } else {
-                    //  в3 Такой ключ есть, доп проверка
-                    //  - ключи имеют разные значения = установить
-                    lineStatus.add(lineAdd("-", curKey, oldValue));
-                    lineStatus.add(lineAdd("+", curKey, newValue));
-                }
+
+            if (newValue.equals(oldValue)) {
+                lineStatus.add(lineAdd(" ", curKey, oldValue));
             } else {
-                //в1 Такого ключа нет во второй коллекции - ключ удален
-                lineStatus.add(lineAdd("-", curKey,
-                        oldValue));
+                lineStatus.add(lineAdd("-", curKey, oldValue));
+                lineStatus.add(lineAdd("+", curKey, newValue));
             }
         }
+
         for (Map.Entry<String, Object> entryNew : newMap.entrySet()) {
             String curKey = entryNew.getKey();
             Object oldValue = oldMap.get(curKey);
-            //2. проверяем, есть ли такой ключ в файле 2
             if (oldValue == null) {
-                //ключ отсутствовал в изначальном
                 lineStatus.add(lineAdd("+", curKey, entryNew.getValue()));
             }
         }
