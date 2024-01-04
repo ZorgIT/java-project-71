b6be5a97659c5c60a768949dc544e4016f54f64c
Zorgit <piligrimexe@Gmail.com>
2024-01-05 00:51:47 +0300

    plain formatter add

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,6 +7,8 @@ import picocli.CommandLine.Option;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 
@@ -71,10 +73,58 @@ public final class App implements Runnable {
                         line.get(2));
             }
             System.out.println("}");
-        } else if (this.format.equals("plane")) {
-            System.out.printf("PLANE OUTPUT NOT IMPLEMENTED");
-        }
+        } else if (this.format.equals("plain")) {
+            StringBuilder sb = new StringBuilder();
+            Iterator<List<String>> iterator = difference.iterator();
+            while (iterator.hasNext()) {
+                List<String> line1 = iterator.next();
+                String lineStatus = line1.get(0);
+                if (lineStatus.equals(" ")) {
+                    continue;
+                }
+                if (!lineStatus.equals(" ")) {
+                    if (lineStatus.equals("-")) {
+                        sb.append("Property " + "\'" + line1.get(1) + "\' ");
+                        List<String> line2 = new ArrayList<>();
+                        if (iterator.hasNext()) {
+                            line2 = iterator.next();
+                        } else {
+                            sb.append("was removed");
+                        }
+                        if (line1.get(1).equals(line2.get(1))) {
+                            sb.append("was updated. From ");
+                            if (line1.get(2).charAt(0) == '['
+                                    || line1.get(2).charAt(0) == '{') {
+                                sb.append("[complex value] ");
+                            } else {
+                                sb.append(line1.get(2) + " ");
+                            }
+                            if (line2.get(2).charAt(0) == '['
+                                    || line1.get(2).charAt(0) == '{') {
+                                sb.append("to [complex value]");
+                            } else {
+                                sb.append("to " + line2.get(2) + " ");
+                            }
+                        } else {
+                            sb.append("was removed");
+                        }
 
+                    } else {
+                        sb.append("Property " + "\'" + line1.get(1) + "\' was"
+                                + " added with value: ");
+                        if (line1.get(2).charAt(0) == '['
+                                || line1.get(2).charAt(0) == '{') {
+                            sb.append("[complex value] ");
+                        } else {
+                            sb.append(line1.get(2) + " ");
+                        }
+
+                    }
+                }
+                sb.append("\n");
+            }
+            System.out.printf(sb.toString());
+        }
     }
 
     /**
